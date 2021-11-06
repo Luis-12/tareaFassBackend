90a89573a11cb2f7070bd469b8ae3abbd8381a8b
Luis-12 <55066676+Luis-12@users.noreply.github.com>
2021-11-06 13:28:59 -0600

    c

---
+++ Artistas/src/api.js
@@ -0,0 +1,87 @@
+
+const fs = require('fs')
+const express = require('express')
+const serverless = require('serverless-http');
+const exp = require('constants');
+ 
+const app = express();
+const router = express.Router();
+app.use(express.json());
+app.use(router);
+app.use('/.netlify/functions/api', router);
+
+module.exports.handler = serverless(app);
+
+
+const port = process.env.PORT || 1337
+let artistas = []
+
+const loadArtistas = () => {
+  fs.readFile('./src/artistas.json', 'utf8', (err, data) => {
+    artistas = JSON.parse(data)
+  });
+}
+loadArtistas()
+
+const saveArtistas = () => {
+  let data = JSON.stringify(artistas,null,2)
+  fs.writeFileSync(__dirname + '/' + 'artistas.json', data)
+}
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Content-Type', 'application/json');
+  next();
+});
+
+router.get('/', (req, res) => {
+  res.json(artistas);
+})
+
+router.get('/:id', (req, res) => {
+  let artista = artistas.find(i => i.id == req.params.id);
+  console.log(req.params.id);
+  if (artista == undefined)
+    res.status(404).send('Artista not found');
+  else
+    res.json(artista);
+})
+
+router.post('/:id', (req, res) => {
+  let index = artistas.findIndex(i => i.id == req.params.id);
+  if (index != -1)
+    res.status(404).send('Artista already exits'); 
+  else {
+    artistas.push(req.body);
+    saveArtistas();
+    res.status(200).send('Artista was Added');
+  }
+})
+
+router.put('/', (req, res) => {
+  let artista= req.body;
+  let index = artistas.findIndex(i => i.id == artista.id);
+  console.log(artista.id);
+  if (index == -1)
+    res.status(404).send('Artista not found');
+  else {
+    artistas[index] = artista;
+    saveArtistas();
+    res.status(200).send('Artista was Updated');
+  }
+})
+
+router.delete('/:id', (req, res) => {
+  let index = artistas.findIndex(i => i.id == req.params.id);
+  if (index == -1)
+    return resolve();
+  else {
+    artistas = artistas.filter(i => i.id != req.params.id);
+    saveArtistas();
+    res.status(200).send('Artista was Added');
+  }
+})
+
