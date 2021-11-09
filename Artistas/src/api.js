f1c631e33217a87a8d020fa951619c005e65784b
Luis Alfredo Vargas Alvarado <55066676+Luis-12@users.noreply.github.com>
2021-11-08 22:36:03 -0600

    Update api.js

--- Artistas/src/api.js
+++ Artistas/src/api.js
@@ -28,14 +28,7 @@ const saveArtistas = () => {
   fs.writeFileSync(__dirname + '/' + 'artistas.json', data)
 }
 
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Content-Type', 'application/json');
-  next();
-});
+
 
 router.get('/', (req, res) => {
    fs.readFile('./src/artistas.json', 'utf8', (err, data) => {
